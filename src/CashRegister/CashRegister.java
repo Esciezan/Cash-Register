f87ca2fa9ff9fa8d74c431d6ffb09a78cca5ceaf
Esciezan <111941519+Esciezan@users.noreply.github.com>
2022-12-12 21:10:29 +0800

    Made some basic GUI

--- src/CashRegister/CashRegister.java
+++ src/CashRegister/CashRegister.java
@@ -1,22 +1,33 @@
 package CashRegister;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
 
 
 public class CashRegister extends JFrame {
-    private JPanel CASH_REGISTER_MAIN_FRAME;
 
-    public CashRegister(String CASH_REGISTER){
-    super(CASH_REGISTER);
-    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    this.setContentPane(CASH_REGISTER_MAIN_FRAME);
-    this.pack();
-            }
-public static void main(String[] args){
-JFrame frame = new CashRegister("Cash Register");
-frame.setVisible(true);
-}
+    private JPanel CASH_REGISTER_MAIN_FRAME;
+    private JTabbedPane BIGPANE;
+    private JTextPane ASDFTextPane;
+    private JPanel FRIES_PANEL;
+    private JPanel BURGER_PANEL;
+    private JButton FRIES_BUTTON;
+    private JButton CURLY_FRIES_BUTTON;
+    private JPanel RECIEPT;
+    private JRadioButton FRIES;
+
+    public CashRegister(String CASH_REGISTER) {
+
+        super(CASH_REGISTER);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(CASH_REGISTER_MAIN_FRAME);
+        this.pack();
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new CashRegister("Cash Register");
+        frame.setSize(500, 500);
+        frame.setVisible(true);
+
+    }
+
 }
